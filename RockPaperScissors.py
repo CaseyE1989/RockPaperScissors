ccfdc777dce11d692fe2b41ffb57e56094c8895b
CaseyE1989 <118781940+CaseyE1989@users.noreply.github.com>
2022-12-24 15:41:57 -0700

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -25,42 +25,47 @@ scissors = '''
 ---.__(___)
 '''
 
-#Write your code below this line 👇
 import random
 print('Welcome to Rock, Paper, Scissors!')
-selected_hand = int(input('What will you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: '))
-cpu_hand = [rock, paper, scissors]
-cpu_random = random.randint(0,2)
+play = True
 
-if selected_hand == 0:
-    print(rock)
-    print('Computer chose:')
-    print(cpu_hand[cpu_random])
-    if cpu_hand[cpu_random] == paper:
-        print('You lose!')
-    elif cpu_hand[cpu_random] == scissors:
-        print('You win!')
-    else:
-        print('Draw!')
-elif selected_hand == 1:
-    print(paper)
-    print('Computer chose:')
-    print(cpu_hand[cpu_random])
-    if cpu_hand[cpu_random] == scissors:
-        print('You lose!')
-    elif cpu_hand[cpu_random] == rock:
-        print('You win!')
-    else:
-        print('Draw!')
-else:
-    print(scissors)
-    print('Computer chose:')
-    print(cpu_hand[cpu_random])
-    if cpu_hand[cpu_random] == rock:
-        print('You lose!')
-    elif cpu_hand[cpu_random] == paper:
-        print('You win!')
-    else:
-        print('Draw!')
+while play:
+    selected_hand = int(input('What will you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: '))
+    cpu_hand = [rock, paper, scissors]
+    cpu_random = random.randint(0,2)
 
+    if selected_hand == 0:
+        print(rock)
+        print('Computer chose:')
+        print(cpu_hand[cpu_random])
+        if cpu_hand[cpu_random] == paper:
+            print('You lose!')
+        elif cpu_hand[cpu_random] == scissors:
+            print('You win!')
+        else:
+            print('Draw!')
+    elif selected_hand == 1:
+        print(paper)
+        print('Computer chose:')
+        print(cpu_hand[cpu_random])
+        if cpu_hand[cpu_random] == scissors:
+            print('You lose!')
+        elif cpu_hand[cpu_random] == rock:
+            print('You win!')
+        else:
+            print('Draw!')
+    else:
+        print(scissors)
+        print('Computer chose:')
+        print(cpu_hand[cpu_random])
+        if cpu_hand[cpu_random] == rock:
+            print('You lose!')
+        elif cpu_hand[cpu_random] == paper:
+            print('You win!')
+        else:
+            print('Draw!')
+    play_again = input(('Do you want to play again? (Y)es or (N)o:  ')).lower()
+    if play_again == 'n':
+        print('Game Over.')
+        play = False
 input()
